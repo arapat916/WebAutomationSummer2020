239f0710708579cd2bb2a62730d3605914793b43
arapat916 <arapat916@gmail.com>
2020-10-30 21:51:05 -0400

    Added firefoxdriver to the Driver class

--- src/test/java/com/weborders/utilities/Driver.java
+++ src/test/java/com/weborders/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.weborders.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
     private Driver (){};
@@ -15,6 +16,10 @@ public static WebDriver getDriver(){
         case "chrome":
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
+            break;
+        case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
             break;
         default:
             throw new RuntimeException("Browser is not implemented yet: "+ browser);
